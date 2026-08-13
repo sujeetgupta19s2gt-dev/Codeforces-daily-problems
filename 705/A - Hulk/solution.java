e3964247170bb715224c77bbc5552ba65922a0c5
sujeetgupta19s2gt-dev <sujeetgupta19s2gt@gmail.com>
2026-08-14 00:38:55 +0530

    Add Hulk [A] from Codeforces

---
+++ 705/A - Hulk/solution.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class hulk{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String oddFeeling = "I hate";
+        String evenFeeling = "I love";
+        String endingLetter = " it";
+        String middleCharacter = " that ";
+        int n = sc.nextInt();
+        String result = "";
+        for(int i=1; i<=n; i++){
+            if(i%2==0){
+                result = result + evenFeeling;
+            }
+            else{
+                result = result + oddFeeling;
+            }
+            if(i==n){
+                result = result + endingLetter;
+            }
+            else{
+                result = result + middleCharacter;
+            }
+        }
+        result = result.trim();
+        System.out.println(result);
+    }
+}
