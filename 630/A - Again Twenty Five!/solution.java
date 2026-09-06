a82934f05099ced691f914e191a76dde7c450e76
sujeetgupta19s2gt-dev <sujeetgupta19s2gt@gmail.com>
2026-09-06 21:14:03 +0530

    Add Again Twenty Five! [A] from Codeforces

---
+++ 630/A - Again Twenty Five!/solution.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextLong()) {
+            long n = sc.nextLong();
+            System.out.println(25);
+        }
+        sc.close();
+    }
+}
