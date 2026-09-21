5a748f52ef0f60a43110dbfa6f4a94a7e17db3bd
Sujeet Gupta <sujeetgupta19s2gt@gmail.com>
2026-09-21 18:42:34 +0530

    Add Line Trip [A] from Codeforces

---
+++ 1901/A - Line Trip/solution.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (!sc.hasNextInt()) return;
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int x = sc.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+ 
+            int maxGap = a[0];
+            for (int i = 1; i < n; i++) {
+                maxGap = Math.max(maxGap, a[i] - a[i - 1]);
+            }
+            maxGap = Math.max(maxGap, 2 * (x - a[n - 1]));
+ 
+            System.out.println(maxGap);
+        }
+        sc.close();
+    }
+}
